f358e5f796ee56e32ba5bf0fcf340e112c6c2e81
venkateshg12 <gvenkatesh1204@gmail.com>
2025-06-20 23:12:52 +0530

    and controller

---
+++ api/src/controller/auth.controller.ts
@@ -0,0 +1,31 @@
+import { register } from "module";
+import catchError from "../utils/catchError";
+import { z } from "zod";
+import { createAccount } from "../services/auth.service";
+import { setAuthCookies } from "../utils/cookies";
+import { CREATED } from "../constants/http";
+
+const registerSchema = z.object({
+    email: z.string().email().min(1).max(255),
+    password: z.string().min(6).max(255),
+    confirmPassword: z.string().min(6).max(255),
+    userAgent: z.string().optional(),
+}).refine((data) => data.password === data.confirmPassword, {
+    "message": "messages do not match",
+    path: ["confirmPassword"],
+})
+
+export const registerHandler = catchError(async (req, res) => {
+    // validate request.
+    const request = registerSchema.parse({
+        ...req.body,
+        userAgent: req.headers["user-agent"],
+    })
+
+    // call service 
+    const { user, refreshToken, accessToken } = await createAccount(request);
+
+    // return response 
+    setAuthCookies({res, accessToken, refreshToken});
+    return res.status(CREATED).json(user);
+})
